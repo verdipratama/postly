325a4ab0d3b8e8ce9eeb5bcf1de362cb2ea218c6
verdipratama <verdipratama@icloud.com>
2020-11-22 02:34:15 +0700

    Fix reset emails

--- resources/views/pages/auth/reset.blade.php
+++ resources/views/pages/auth/reset.blade.php
@@ -13,7 +13,7 @@
           <div class="custom-form">
             <form method="POST" action="">
               <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">YOUR E-MAIL</label>
+                <label for="email" class="col-md-4 col-form-label text-md-right">Masukan Email</label>
                 <div class="col-md-6">
                   <input id="email" type="email" class="form-control" name="email" value="" required
                     autocomplete="email" autofocus>
@@ -22,7 +22,7 @@
               <div class="form-group row mb-0">
                 <div class="col-md-6 offset-md-4">
                   <button type="submit" class="btn">
-                    TESSS
+                    Submit
                   </button>
                 </div>
               </div>
