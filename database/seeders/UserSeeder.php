bd99692eda003aec9444c48c07592bc907c4d1bc
verdipratama <verdipratama@icloud.com>
2020-11-25 22:27:32 +0700

    Adding users seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => 'Verdi',
+            'last_name'  => 'Pratama',
+            'username'   => 'verdipratama',
+            'email'      => 'verdipratama@yahoo.com',
+            'password'   => Hash::make('123456'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'is_admin'   => '1',
+        ]);
+    }
+}
