52680da82da29647f8ba02b06c1d898b8a3990c0
verdipratama <verdipratama@icloud.com>
2020-11-29 03:34:18 +0700

    New UI

--- resources/views/pages/upload.blade.php
+++ resources/views/pages/upload.blade.php
@@ -4,14 +4,36 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <div class="page-desctiption">
-          <h1>Ini upload page Middleware</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, corrupti quisquam illo doloribus excepturi
-            necessitatibus nemo? Amet fuga illum laudantium nam ipsum? Quia maiores corporis temporibus neque mollitia
-            consequatur eveniet?
-            <br>
-            <code>---Login to access this page---</code>
-          </p>
+        <div class="video-upload-title">
+          <h2>What's hot?</h2>
+          <p class="percentence">---Login to access this page---</p>
+          <div class="custom-form">
+            <div class="row">
+              <div class="col-lg-6 mx-auto">
+                <form action="" method="POST" id="submit_post">
+                  <input type="hidden" name="" value=""><input type="hidden" id="post_url" value="">
+                  <div class="form-group">
+                    <textarea name="caption" oninput="mycaption()" id="caption" class="form-control" cols="30"
+                      rows="10"></textarea>
+                  </div>
+                  <div class="video-public-privet-action">
+                    <div class="custom-control custom-radio custom-control-inline">
+                      <input type="radio" checked="" id="public" name="status" class="custom-control-input"
+                        value="1"><label class="custom-control-label" for="public">Public</label>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline">
+                      <input type="radio" id="privet" name="status" class="custom-control-input" value="0"><label
+                        class="custom-control-label" for="privet">Privet</label>
+                    </div>
+                  </div>
+                  <div class="submit-post-action f-right">
+                    <a href="#">Cancel</a>
+                    <button class="disabled" id="upload_btn" type="submit">Post</button>
+                  </div>
+                </form>
+              </div><!-- /.col-lg-6 -->
+            </div><!-- /.row -->
+          </div><!-- /.custom-form -->
         </div>
       </div>
     </div>
