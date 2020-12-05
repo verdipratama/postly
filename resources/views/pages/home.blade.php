143ca2b1d87d9f139c18c0cb174e76b5ea1200b6
verdipratama <verdipratama@icloud.com>
2020-12-05 08:36:00 +0700

    create UI

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,31 +2,55 @@
 @section('content')
 
 <div class="main-area pt-50">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="page-desctiption">
-          <div class="jumbotron mt-10">
-            <h1 class="display-4">Selamat Datang</h1>
-            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt alias non, dolorum
-              tenetur perferendis est accusantium distinctio, sed nostrum voluptatem, totam ab iure commodi? Nostrum
-              consectetur voluptatem non ducimus!</p>
-            <hr class="my-2">
-            <p class="lead">
-              @auth
-              <a class="btn btn-primary btn-lg pjax" href="/" role="button">Hello!
-                {{ auth()->user()->first_name }}</a>
-              @endauth
-              @guest
-              <a class="btn btn-primary btn-lg pjax" href="{{ route('register') }}" role="button">Registrasi
-                sekarang!</a>
-              @endguest
-            </p>
+
+  <div class="text-center">
+    @auth
+    <img src="{{ asset('frontend/img/users.png') }}" alt class="ui-w-100 rounded-circle">
+    <div class="col-md-8 col-lg-6 col-xl-5 p-0 mx-auto">
+      <h4 class="font-weight-bold my-4">{{ auth()->user()->first_name }}</h4>
+      <div class="text-muted mb-4">
+        {{ __('a senior UX Developer with five years of industry experience!') }}
+      </div>
+    </div>
+    @endauth
+
+    @guest
+    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt class="ui-w-100 rounded-circle">
+    <div class="col-md-8 col-lg-6 col-xl-5 p-0 mx-auto">
+      <h4 class="font-weight-bold my-4">{{ __('John Doe') }}</h4>
+      <div class="text-muted mb-4">
+        {{ __('a senior UX Developer with five years of industry experience!') }}
+      </div>
+    </div>
+    @endguest
+  </div>
+
+  <div class="page-content container note-has-grid">
+    <div class="tab-content bg-transparent">
+      <div id="note-full-container" class="note-has-grid row">
+
+        <div class="col-md-4 single-note-item all-category note-important">
+          <div class="card card-body">
+            <h5 class="note-title text-truncate w-75 mb-0">
+              {{ __('@johndoe') }}
+            </h5>
+            <p class="note-date font-12 text-muted">{{ __('4 Fabruary 2021') }}</p>
+            <div class="note-content">
+              <p class="note-inner-content text-muted">
+                {{ __('Blandit tempus porttitor aasfs. Integer posuere erat a ante
+                venenatis.') }}
+              </p>
+            </div>
+            <div class="d-flex align-items-center">
+              <span class="mr-1"><i class="fa fa-star favourite-note"></i></span>
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   </div>
+
 </div>
 
 @endsection
