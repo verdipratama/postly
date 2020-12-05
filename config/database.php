f9da890e4399c84d616d25c866e858558e048b42
verdipratama <verdipratama@icloud.com>
2020-12-05 21:11:11 +0700

    config heroku db connection

--- config/database.php
+++ config/database.php
@@ -17,8 +17,8 @@ return [
     |
      */
 
-    'default'     => env('DB_CONNECTION', 'mysql'),
-
+    // 'default'     => env('DB_CONNECTION', 'mysql'),
+    'default'     => env('DB_CONNECTION', 'pgsql'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
