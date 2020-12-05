62b8304a9838e436f255fc3a07f6709ef19956a0
verdipratama <verdipratama@icloud.com>
2020-12-06 02:53:02 +0700

    config heroku seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,24 +16,36 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(array(0 => array(
-            'first_name' => 'Verdi',
-            'last_name'  => 'Pratama',
-            'username'   => 'verdipratama',
-            'email'      => 'verdipratama@yahoo.com',
-            'password'   => Hash::make('123456'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'is_admin'   => 1,
-        ), 1 => array(
-            'first_name' => 'John',
-            'last_name'  => 'Doe',
-            'username'   => 'johndoe',
-            'email'      => 'johndoe@gmail.com',
-            'password'   => Hash::make('123456'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'is_admin'   => 2,
-        )));
+        DB::table('users')->insert(
+            [
+                'first_name' => 'Verdi',
+                'last_name'  => 'Pratama',
+                'username'   => 'verdipratama',
+                'email'      => 'verdipratama@yahoo.com',
+                'password'   => Hash::make('123456'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'is_admin'   => '1',
+            ]
+        );
+        // DB::table('users')->insert(array(0 => array(
+        //     'first_name' => 'Verdi',
+        //     'last_name'  => 'Pratama',
+        //     'username'   => 'verdipratama',
+        //     'email'      => 'verdipratama@yahoo.com',
+        //     'password'   => Hash::make('123456'),
+        //     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //     'is_admin'   => '1',
+        // ), 1 => array(
+        //     'first_name' => 'John',
+        //     'last_name'  => 'Doe',
+        //     'username'   => 'johndoe',
+        //     'email'      => 'johndoe@gmail.com',
+        //     'password'   => Hash::make('123456'),
+        //     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //     'is_admin'   => '2',
+        // )));
     }
 }
