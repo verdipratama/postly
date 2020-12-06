58383a2596969445f32e9735bee5955247c34737
verdipratama <verdipratama@icloud.com>
2020-12-07 05:12:08 +0700

    Clean up db

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
      */
 
-    'default'     => env('DB_CONNECTION', 'mysql', 'pgsql'),
+    'default'     => env('DB_CONNECTION', 'pgsql', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
